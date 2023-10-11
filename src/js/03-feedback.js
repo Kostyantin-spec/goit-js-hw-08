5ce12c406500aedd624f9da0a40b67e1c53f338f
Kostyantin-spec <134406433+Kostyantin-spec@users.noreply.github.com>
2023-10-11 19:23:15 +0300

    fjkhuiy

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -10,17 +10,7 @@ const refs = {
     email: document.querySelector('input[name="email"]'),
 };
 
-refs.form.addEventListener('email', throttle(onFormSubmit, 500));
-//refs.textarea.addEventListener('message', throttle(onTextareaInput, 500));
-
-// refs.form.addEventListener('input',e => {
-//     consolr.log(e.target.name);
-//     console.log(e.target.value);
-
-//     formData[e.target.name] = e.target.value;
-//     localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-    
-// });
+refs.form.addEventListener('submit', throttle(onFormSubmit, 500));
 
 refs.form.addEventListener('input', throttle(onFormInput,500));
 function onFormInput(e) {
@@ -28,16 +18,7 @@ function onFormInput(e) {
     localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 
-
-populateTextarea();
-
-
-
-
-//Останавливаем поведение по умалчанию
-//Убираем сообщение из хранилища
-//Очищаем форму
-
+window.addEventListener("load", populateTextarea);
 
 function onFormSubmit(evt) {
     evt.preventDefault();
@@ -46,33 +27,16 @@ function onFormSubmit(evt) {
     refs.email.value = "";
     evt.currentTarget.reset();
     localStorage.removeItem(STORAGE_KEY);
+    console.log(formData);
 }
 
-//Получить значение поля
-//Сохранить его в хранилище
-
-// function onTextareaInput(evt) {
-//     const message = evt.target.value;
-
-//     localStorage.setItem(STORAGE_KEY, message);
-// }
-
-//Получаем значение из хранилеща
-//Если там чтото біло, обновляем DOM
-
 function populateTextarea() {
-    const savedData = localStorage.getItem(STORAGE_KEY);
+    const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
 
     if (savedData !== null) {   
-    //console.log(savedData);
-    refs.textarea.value = savedData;
+    console.log(savedData);
+    refs.textarea.value = savedData.message;
     refs.email.value = savedData.email;
     }
 }
 
-// function initForm() {
-//     if (persistedFilters) {
-//         persistedFilters = JSON.parse(persistedFilters);
-//         console.log(persistedFilters);
-//     }
-// }
